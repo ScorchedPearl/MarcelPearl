d6254c330dba4df8edab17eb4a12c130562e0b70
ScorchedPearl <iit2024087@iiita.ac.in>
2025-03-17 18:28:45 +0530

    Init

---
+++ backend/src/main/java/com/MarcellaKaizo/MarcelPearl/MarcelPearlApplication.java
@@ -0,0 +1,13 @@
+package com.MarcellaKaizo.MarcelPearl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcelPearlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcelPearlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MarcellaKaizo/MarcelPearl/MarcelPearlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MarcellaKaizo.MarcelPearl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcelPearlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
